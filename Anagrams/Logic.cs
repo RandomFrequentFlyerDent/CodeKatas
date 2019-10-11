2398b31d3f05380e01978be39df99f53b88542bc
Nathalie Everts <n.everts@gmail.com>
2019-10-11 21:58:15 +0200

    Logic + tests

---
+++ Anagrams/Logic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anagrams
+{
+    public static class Logic
+    {
+        public static Dictionary<string, List<string>> GetAnagrams(List<string> words)
+        {
+            var anagrams = new Dictionary<string, List<string>>();
+
+            if (words.Any())
+            {
+                foreach (var word in words)
+                {
+                    anagrams.Add(word, GetAnagrams(word, words));
+                }
+            }
+
+            return anagrams;
+        }
+
+        private static List<string> GetAnagrams(string word, List<string> words)
+        {
+            var tempWords = words.ToList();
+            tempWords.Remove(word);
+            var tempWord = String.Concat(word.OrderBy(c => c));
+            return tempWords.Where(w => String.Concat(w.OrderBy(c => c)).Equals(tempWord)).ToList();
+        }
+    }
+
+}
--- CodeKatasTests/AnagramTests.cs
+++ CodeKatasTests/AnagramTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Anagrams;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace CodeKatasTests
 {
@@ -6,9 +8,75 @@ namespace CodeKatasTests
     public class AnagramTests
     {
         [Test]
-        public void ReturnsNoAnagrams()
+        public void EmptyList()
         {
+            var words = new List<string>();
+            var actual = Logic.GetAnagrams(words);
+            var expected = new Dictionary<string, List<string>>();
 
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OneWord()
+        {
+            var words = new List<string> { "apple" };
+            var actual = Logic.GetAnagrams(words);
+            var expected = new Dictionary<string, List<string>>();
+            expected.Add("apple", new List<string>());
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoWordsNoAnagrams()
+        {
+            var words = new List<string> { "apple", "pear" };
+            var actual = Logic.GetAnagrams(words);
+            var expected = new Dictionary<string, List<string>>();
+            expected.Add("apple", new List<string>());
+            expected.Add("pear", new List<string>());
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoWordsBothAnagrams()
+        {
+            var words = new List<string> { "done", "node" };
+            var actual = Logic.GetAnagrams(words);
+            var expected = new Dictionary<string, List<string>>();
+            expected.Add("done", new List<string>() { "node" });
+            expected.Add("node", new List<string>() { "done" });
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FourWordsTwoAnagrams()
+        {
+            var words = new List<string> { "done", "node", "apple", "pear" };
+            var actual = Logic.GetAnagrams(words);
+            var expected = new Dictionary<string, List<string>>();
+            expected.Add("done", new List<string>() { "node" });
+            expected.Add("node", new List<string>() { "done" });
+            expected.Add("apple", new List<string>());
+            expected.Add("pear", new List<string>());
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AllWordsAnagrams()
+        {
+            var words = new List<string> { "skin", "sink", "kins" };
+            var actual = Logic.GetAnagrams(words);
+            var expected = new Dictionary<string, List<string>>();
+            expected.Add("skin", new List<string>() { "sink", "kins" });
+            expected.Add("sink", new List<string>() { "skin", "kins" });
+            expected.Add("kins", new List<string>() { "skin", "sink" });
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
